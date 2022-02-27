07c916be34236ff10bf972975b1dcdca6115be1b
caH40 <kmvsancho@gmail.com>
2022-02-27 22:40:36 +0300

    added 2 webcam

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const download = require('image-downloader');
 const path = require('path');
 var CronJob = require('cron').CronJob;
-
-var job = new CronJob('*/1 * * * *', async function () {
-	await screenDownLoad();
+var index = 0;
+var job = new CronJob('*/3 * * * *', async function () {
+	index++
+	await screenDownLoad(index, 'images/web1', process.env.URL_WEB1);
+	await screenDownLoad(index, 'images/web5', process.env.URL_WEB5);
 });
 job.start();
 
-async function screenDownLoad() {
+async function screenDownLoad(index, dir, url) {
 	try {
 		let options = {
-			url: process.env.URL,
-			dest: `${path.join(__dirname, 'images')}/${new Date().getTime()}.jpg`
+			url: url,
+			dest: `${path.join(__dirname, dir)}/${index}.jpg`
 		};
 		await download.image(options)
 			.catch((error) => console.log(error))
